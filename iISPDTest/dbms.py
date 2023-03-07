d1cac2ff52da446267228c3ec9ffd8dbe7a2da11
Chandrashekar V <64350441+chandrashekarv2000@users.noreply.github.com>
2023-03-07 09:52:47 +0530

    Delete dbms.py

--- iISPDTest/dbms.py
+++
@@ -1,4 +0,0 @@
-import mysql.connector
-from mysql.connector.authentication import get_auth_plugin
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="tiger",auth_plugin="mysql_native_password")
-print(mydb)
